7d44efc7846264f811eb7459e50218c7eab8c35c
piskvorky <radimrehurek@seznam.cz>
2011-12-02 05:02:49 +0100

    renamed readme & up version (1.0.8)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(fname):
 
 setup(
     name = 'sqlitedict',
-    version = '1.0.7',
+    version = '1.0.8',
     description = 'Persistent dict in Python, backed up by sqlite3 and pickle, multithread-safe.',
     long_description = read('README.txt'),
 
