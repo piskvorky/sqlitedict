f0ebeb6a5a443732bf17d01de1247fc5aaf118c1
Radim Řehůřek <radimrehurek@seznam.cz>
2014-06-01 12:13:22 +0200

    create test dir if missing

--- tests/test_named_db.py
+++ tests/test_named_db.py
@@ -1,21 +1,29 @@
 import os
-import unittest
 import sqlitedict
 
 from test_temp_db import TempSqliteDictTest
 
 
+def norm_file(fname):
+    """Normalize test filename, creating a directory path to it if necessary"""
+    fname = os.path.abspath(fname)
+    dirname = os.path.dirname(fname)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    return fname
+
+
 class NamedSqliteDictTest(TempSqliteDictTest):
 
     def setUp(self):
-        db = os.path.abspath('tests/db/sqlitedict-with-def.sqlite')
+        db = norm_file('tests/db/sqlitedict-with-def.sqlite')
         self.d = sqlitedict.SqliteDict(filename=db)
 
 
 class CreateNewSqliteDictTest(TempSqliteDictTest):
 
     def setUp(self):
-        db = os.path.abspath('tests/db/sqlitedict-with-n-flag.sqlite')
+        db = norm_file('tests/db/sqlitedict-with-n-flag.sqlite')
         self.d = sqlitedict.SqliteDict(filename=db, flag="n")
 
     def tearDown(self):
@@ -25,7 +33,7 @@ class CreateNewSqliteDictTest(TempSqliteDictTest):
 class StartsWithEmptySqliteDictTest(TempSqliteDictTest):
 
     def setUp(self):
-        db = os.path.abspath('tests/db/sqlitedict-with-w-flag.sqlite')
+        db = norm_file('tests/db/sqlitedict-with-w-flag.sqlite')
         self.d = sqlitedict.SqliteDict(filename=db, flag="w")
 
     def tearDown(self):
@@ -36,7 +44,7 @@ class StartsWithEmptySqliteDictTest(TempSqliteDictTest):
 class SqliteDictAutocommitTest(TempSqliteDictTest):
 
     def setUp(self):
-        db = os.path.abspath('tests/db/sqlitedict-autocommit.sqlite')
+        db = norm_file('tests/db/sqlitedict-autocommit.sqlite')
         self.d = sqlitedict.SqliteDict(filename=db, autocommit=True)
 
     def tearDown(self):
