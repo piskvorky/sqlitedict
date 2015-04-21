7f3e50fe96a3bdb104fd3e2012befda0c4ce35aa
Jeff Quast <jquast@io.com>
2015-04-20 20:38:04 -0700

    use six.py's reraise() for py27/34 compatibility

--- sqlitedict.py
+++ sqlitedict.py
@@ -38,8 +38,32 @@ from threading import Thread
 
 _major_version = sys.version_info[0]
 if _major_version < 3: # py <= 2.x
-  if sys.version_info[1] < 5: # py <= 2.4
-    raise ImportError("sqlitedict requires python 2.5 or higher (python 3.3 or higher supported)")
+    if sys.version_info[1] < 5: # py <= 2.4
+      raise ImportError("sqlitedict requires python 2.5 or higher (python 3.3 or higher supported)")
+
+    # necessary to use exec()_ as this would be a SyntaxError in python3.
+    # this is an exact port of six.reraise():
+    def exec_(_code_, _globs_=None, _locs_=None):
+        """Execute code in a namespace."""
+        if _globs_ is None:
+            frame = sys._getframe(1)
+            _globs_ = frame.f_globals
+            if _locs_ is None:
+                _locs_ = frame.f_locals
+            del frame
+        elif _locs_ is None:
+            _locs_ = _globs_
+        exec("""exec _code_ in _globs_, _locs_""")
+
+    exec_("def reraise(tp, value, tb=None):\n"
+          "    raise tp, value, tb\n")
+else:
+    def reraise(tp, value, tb=None):
+        if value is None:
+            value = tp()
+        if value.__traceback__ is not tb:
+            raise value.with_traceback(tb)
+        raise value
 
 try:
     from cPickle import dumps, loads, HIGHEST_PROTOCOL as PICKLE_PROTOCOL
@@ -378,7 +402,7 @@ class SqliteMultithread(Thread):
             # as `pdb', or simply evaluating the naturally raised traceback, we
             # retain the original (inner) location of where the exception
             # occurred.
-            raise e_type, e_value, e_tb
+            reraise(e_type, e_value, e_tb)
 
 
     def execute(self, req, arg=None, res=None):
