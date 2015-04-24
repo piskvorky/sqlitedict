98ee0d2e9dab94c9219c3610ed0448a8086123df
Jeff Quast <jquast@io.com>
2015-04-23 17:43:59 -0700

    rename test_temp_db => test_valuetypes
    move picklable key type tests to test_keytypes.py

--- tests/test_valuetypes.py
+++ tests/test_valuetypes.py
@@ -25,8 +25,8 @@ class TempSqliteDictTest(TestCaseBackport):
     def test_assign_values(self):
         ''' test_assign_values
         '''
-        self.d['abc'] = 'edf'
-        self.assertEqual(self.d['abc'], 'edf')
+        self.d[u'abc'] = u'edf'
+        self.assertEqual(self.d[u'abc'], u'edf')
         self.assertEqual(len(self.d), 1)
 
     def test_clear_data(self):
@@ -40,58 +40,40 @@ class TempSqliteDictTest(TestCaseBackport):
     def test_manage_one_record(self):
         ''' test_manage_one_record
         '''
-        self.d['abc'] = 'rsvp' * 100
-        self.assertEqual(self.d['abc'], 'rsvp' * 100)
-        self.d['abc'] = 'lmno'
-        self.assertEqual(self.d['abc'], 'lmno')
+        self.d[u'abc'] = u'rsvp' * 100
+        self.assertEqual(self.d[u'abc'], u'rsvp' * 100)
+        self.d[u'abc'] = u'lmno'
+        self.assertEqual(self.d[u'abc'], u'lmno')
         self.assertEqual(len(self.d), 1)
-        del self.d['abc']
+        del self.d[u'abc']
         self.assertEqual(len(self.d), 0)
         self.assertTrue(not self.d)
 
     def test_manage_few_records(self):
         ''' test_manage_few_records
         '''
-        self.d['abc'] = 'lmno'
-        self.d['xyz'] = 'pdq'
+        self.d[u'abc'] = u'lmno'
+        self.d[u'xyz'] = u'pdq'
         self.assertEqual(len(self.d), 2)
         if _major_version == 2:
             self.assertEqual(list(self.d.iteritems()),
-                            [('abc', 'lmno'), ('xyz', 'pdq')])
+                            [(u'abc', u'lmno'), (u'xyz', u'pdq')])
         self.assertEqual(self.d.items(),
-                        [('abc', 'lmno'), ('xyz', 'pdq')])
-        self.assertEqual(self.d.values(), ['lmno', 'pdq'])
-        self.assertEqual(self.d.keys(), ['abc', 'xyz'])
-        self.assertEqual(list(self.d), ['abc', 'xyz'])
-
-    def test_records_with_picklable_key(self):
-        ''' test_records_with_picklable_key
-        '''
-        self.d[["abc", "klm"]] = "xyz"
-        self.d[("cde", "ftp")] = 35
-        self.d[{"a": 3, "b": 6}] = "yse"
-        self.assertEqual(len(self.d), 3)
-        if _major_version == 2:
-            self.assertEqual(list(self.d.iteritems()),
-                            [(["abc", "klm"], "xyz"), (("cde", "ftp"), 35),
-                             ({"a": 3, "b": 6}, "yse")])
-        self.assertEqual(self.d.items(),
-                        [(["abc", "klm"], "xyz"), (("cde", "ftp"), 35),
-                             ({"a": 3, "b": 6}, "yse")])
-        self.assertEqual(self.d.values(), ["xyz", 35, "yse"])
-        self.assertEqual(self.d.keys(), [["abc", "klm"], ("cde", "ftp"), {"a": 3, "b": 6}])
-        self.assertEqual(list(self.d), [["abc", "klm"], ("cde", "ftp"), {"a": 3, "b": 6}])
+                        [(u'abc', u'lmno'), (u'xyz', u'pdq')])
+        self.assertEqual(self.d.values(), [u'lmno', u'pdq'])
+        self.assertEqual(self.d.keys(), [u'abc', u'xyz'])
+        self.assertEqual(list(self.d), [u'abc', u'xyz'])
 
     def test_update_records(self):
         ''' test_update_records
         '''
-        self.d.update(p='x', q='y', r='z')
+        self.d.update(p=u'x', q=u'y', r=u'z')
         self.assertEqual(len(self.d), 3)
         # As far as I know dicts does not need to return
         # the elements in a specified order (sort() is required )
         self.assertEqual(sorted(self.d.items()),
-                        sorted([('q', 'y'), ('p', 'x'), ('r', 'z')]))
-        self.assertEqual(sorted(list(self.d)), sorted(['q', 'p', 'r']))
+                        sorted([(u'q', u'y'), (u'p', u'x'), (u'r', u'z')]))
+        self.assertEqual(sorted(list(self.d)), sorted([u'q', u'p', u'r']))
 
     def test_handling_errors(self):
         ''' test_handling_errors
@@ -103,6 +85,6 @@ class TempSqliteDictTest(TestCaseBackport):
             del d[k]
 
         with self.assertRaises(KeyError):
-            remove_nonexists(self.d, 'abc')
+            remove_nonexists(self.d, u'abc')
         with self.assertRaises(KeyError):
-            get_value(self.d, 'abc')
+            get_value(self.d, u'abc')
