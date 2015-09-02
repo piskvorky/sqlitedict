8e8a62761ac8a9bc5604e9c5ae70c8739862ab83
Radim Řehůřek <radimrehurek@seznam.cz>
2015-09-02 23:47:54 +0900

    formatting fixes in setup.py

--- setup.py
+++ setup.py
@@ -13,18 +13,19 @@ python ./setup.py install
 """
 
 import os
-
+import io
 import subprocess
-import setuptools.command.develop
 
+import setuptools.command.develop
 from distutils.core import setup
 
+
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    path = os.path.join(os.path.dirname(__file__), fname)
+    return io.open(path, encoding='utf8').read()
 
 
 class SetupDevelop(setuptools.command.develop.develop):
-
     """Docstring is overwritten."""
 
     def run(self):
@@ -43,31 +44,30 @@ class SetupDevelop(setuptools.command.develop.develop):
 SetupDevelop.__doc__ = setuptools.command.develop.develop.__doc__
 
 
-
 setup(
-    name = 'sqlitedict',
-    version = '1.3.0',
-    description = 'Persistent dict in Python, backed up by sqlite3 and pickle, multithread-safe.',
-    long_description = read('README.rst'),
+    name='sqlitedict',
+    version='1.4.0',
+    description='Persistent dict in Python, backed up by sqlite3 and pickle, multithread-safe.',
+    long_description=read('README.rst'),
 
-    py_modules = ['sqlitedict'],
+    py_modules=['sqlitedict'],
 
     # there is a bug in python2.5, preventing distutils from using any non-ascii characters :(
     # http://bugs.python.org/issue2562
-    author = 'Radim Rehurek, Victor R. Escobar, Andrey Usov, Prasanna Swaminathan, Jeff Quast',
-    author_email = "various",
-    maintainer = 'Radim Rehurek',
-    maintainer_email = 'me@radimrehurek.com',
+    author='Radim Rehurek, Victor R. Escobar, Andrey Usov, Prasanna Swaminathan, Jeff Quast',
+    author_email="various",
+    maintainer='Radim Rehurek',
+    maintainer_email='me@radimrehurek.com',
 
-    url = 'https://github.com/piskvorky/sqlitedict',
-    download_url = 'http://pypi.python.org/pypi/sqlitedict',
+    url='https://github.com/piskvorky/sqlitedict',
+    download_url='http://pypi.python.org/pypi/sqlitedict',
 
-    keywords = 'sqlite, persistent dict, multithreaded',
+    keywords='sqlite, persistent dict, multithreaded',
 
-    license = 'Apache 2.0',
-    platforms = 'any',
+    license='Apache 2.0',
+    platforms='any',
 
-    classifiers = [ # from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[  # from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
