d345a670f599d87fcd3aee862cd514c572753a02
Rolf Campbell <campbell.rolf@gmail.com>
2019-12-29 11:25:52 -0500

    Avoid adding a symlink (use PYTHONPATH instead). Inline the autocommit2.py file (reduce file count).

--- tests/autocommit2.py
+++
@@ -1,6 +0,0 @@
-import sqlitedict
-
-d = sqlitedict.SqliteDict('tests/db/autocommit.sqlite', autocommit=True )
-
-for i in range(1000):
-    assert d[i] == i
--- tests/test_autocommit.py
+++ tests/test_autocommit.py
@@ -2,5 +2,11 @@ import sys, os
 
 def test():
     "Verify autocommit just before program exits."
-    assert(0 == os.system('%s tests/autocommit.py' % sys.executable))
-    assert(0 == os.system('%s tests/autocommit2.py' % sys.executable))
+    assert(0 == os.system('PYTHONPATH=. %s tests/autocommit.py' % sys.executable))
+    # The above script relies on the autocommit feature working correctly.
+    # Now, let's check if it actually worked.
+    import sqlitedict
+    d = sqlitedict.SqliteDict('tests/db/autocommit.sqlite')
+    expected = {str(i): i for i in range(1000)}
+    actual = {key: value for (key, value) in d.items()}
+    assert expected == actual, [expected, actual]
