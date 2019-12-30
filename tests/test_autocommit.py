39e2aded760389ea14bd18be88f7b13ef2ab08ad
Rolf Campbell <campbell.rolf@gmail.com>
2019-12-30 17:02:40 -0500

    Move import to module level. Use %s instead of %d in assert string.

--- tests/test_autocommit.py
+++ tests/test_autocommit.py
@@ -1,11 +1,11 @@
 import sys, os
+import sqlitedict
 
 def test():
     "Verify autocommit just before program exits."
     assert os.system('PYTHONPATH=. %s tests/autocommit.py' % sys.executable) == 0
     # The above script relies on the autocommit feature working correctly.
     # Now, let's check if it actually worked.
-    import sqlitedict
     d = sqlitedict.SqliteDict('tests/db/autocommit.sqlite')
     for i in range(1000):
-        assert d[i] == i, "actual: %d expected: %d" % (d[i], i)
+        assert d[i] == i, "actual: %s expected: %s" % (d[i], i)
