e9489abac1024d3f84c6b68eaa30b9b8d349a9b2
Michael Penkov <m@penkov.dev>
2022-03-04 22:06:53 +0900

    bump version to 2.0.0

--- sqlitedict.py
+++ sqlitedict.py
@@ -36,39 +36,16 @@ import traceback
 
 from threading import Thread
 
-__version__ = '1.7.0.dev0'
+__version__ = '2.0.0'
 
-major_version = sys.version_info[0]
-if major_version < 3:  # py <= 2.x
-    if sys.version_info[1] < 5:  # py <= 2.4
-        raise ImportError("sqlitedict requires python 2.5 or higher (python 3.3 or higher supported)")
 
-    # necessary to use exec()_ as this would be a SyntaxError in python3.
-    # this is an exact port of six.reraise():
-    def exec_(_code_, _globs_=None, _locs_=None):
-        """Execute code in a namespace."""
-        if _globs_ is None:
-            frame = sys._getframe(1)
-            _globs_ = frame.f_globals
-            if _locs_ is None:
-                _locs_ = frame.f_locals
-            del frame
-        elif _locs_ is None:
-            _locs_ = _globs_
-        exec("""exec _code_ in _globs_, _locs_""")
+def reraise(tp, value, tb=None):
+    if value is None:
+        value = tp()
+    if value.__traceback__ is not tb:
+        raise value.with_traceback(tb)
+    raise value
 
-    class TimeoutError(OSError):
-        pass
-
-    exec_("def reraise(tp, value, tb=None):\n"
-          "    raise tp, value, tb\n")
-else:
-    def reraise(tp, value, tb=None):
-        if value is None:
-            value = tp()
-        if value.__traceback__ is not tb:
-            raise value.with_traceback(tb)
-        raise value
 
 try:
     from cPickle import dumps, loads, HIGHEST_PROTOCOL as PICKLE_PROTOCOL
@@ -248,13 +225,13 @@ class SqliteDict(DictClass):
             yield key, self.decode(value)
 
     def keys(self):
-        return self.iterkeys() if major_version > 2 else list(self.iterkeys())
+        return self.iterkeys()
 
     def values(self):
-        return self.itervalues() if major_version > 2 else list(self.itervalues())
+        return self.itervalues()
 
     def items(self):
-        return self.iteritems() if major_version > 2 else list(self.iteritems())
+        return self.iteritems()
 
     def __contains__(self, key):
         HAS_ITEM = 'SELECT 1 FROM "%s" WHERE key = ?' % self.tablename
