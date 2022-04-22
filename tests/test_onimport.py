c2588d9004176779a78a04811a6caa9e132e66eb
Michael Penkov <m@penkov.dev>
2022-04-22 21:58:41 +0900

    torch obsolete test

--- tests/test_onimport.py
+++ tests/test_onimport.py
@@ -39,22 +39,3 @@ class SqliteDict_cPickleImportTest(unittest.TestCase):
         # verify,
         self.assertIn('pickle', sys.modules.keys())
         self.assertIs(sqlitedict.dumps, sys.modules['pickle'].dumps)
-
-
-class SqliteDictPython24Test(unittest.TestCase):
-    """Verify ImportError when using python2.4 or earlier."""
-    def setUp(self):
-        # manually monkeypatch sys.version_info
-        self._orig_version_info = sys.version_info
-        sys.version_info = (2, 4, 0, 'does-not-matter', 0)
-        self.orig_sqlitedict = sys.modules.pop('sqlitedict', None)
-
-    def tearDown(self):
-        # remove our monkeypatch
-        sys.version_info = self._orig_version_info
-        if self.orig_sqlitedict:
-            sys.modules['sqlitedict'] = self.orig_sqlitedict
-
-    def test_py24_error(self):
-        with self.assertRaises(ImportError):
-            __import__("sqlitedict")
