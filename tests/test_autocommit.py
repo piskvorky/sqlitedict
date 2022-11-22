32d8604b3a2a2185c8e48d67b2045ac3b0d37edc
Cologler <skyoflw@gmail.com>
2022-11-23 03:18:34 +0800

    allow the tests run on windows

--- tests/test_autocommit.py
+++ tests/test_autocommit.py
@@ -6,7 +6,7 @@ import sqlitedict
 
 def test():
     "Verify autocommit just before program exits."
-    assert os.system('PYTHONPATH=. %s tests/autocommit.py' % sys.executable) == 0
+    assert os.system('env PYTHONPATH=. %s tests/autocommit.py' % sys.executable) == 0
     # The above script relies on the autocommit feature working correctly.
     # Now, let's check if it actually worked.
     d = sqlitedict.SqliteDict('tests/db/autocommit.sqlite')
